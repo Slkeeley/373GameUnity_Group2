ab2d645649124d5f049300deb0940c250b1ac6de
Samuel Keeley <83038330+Slkeeley@users.noreply.github.com>
2022-04-17 19:04:34 -0700

    FPS Controller New

--- Assets/Scripts/TracerAbilities.cs
+++ Assets/Scripts/TracerAbilities.cs
@@ -5,16 +5,24 @@ using UnityEngine;
 public class TracerAbilities : MonoBehaviour
 {
 
-    private Vector3 currPos; 
+    public Vector3 currpos; 
         // Start is called before the first frame update
     void Start()
     {
-        
+        currpos = this.transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-      
+
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+           {
+            Debug.Log("blink");
+            transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z+7.8f);
+            }
+
+        
+
     }
 }
